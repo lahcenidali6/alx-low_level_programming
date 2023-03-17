b2f2da0b44eb27ef8e9c8b4ab20953113aa46f4d
lahcenidali <lehcenidali6@gmail.com>
2023-03-17 08:52:31 -0700

    C program that prints random speach

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
+/**
+ *print Programming is like building a multilingual puzzle
+ * */
 int main(void) 
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);}
+	return (0);
+}
