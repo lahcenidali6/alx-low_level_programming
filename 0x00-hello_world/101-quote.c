b6b8707ac1c5f97eaf3fe2c1a4f7bb30ecba9970
lahcenidali <lehcenidali6@gmail.com>
2023-03-17 09:57:49 -0700

    print random speach

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point, print Programming is like building a multilingual puzzle
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n")
+	return (1);
+}
