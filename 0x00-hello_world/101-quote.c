3d9ecd68ce020a672b2b04c2b32285993b6d5046
lahcenidali <lehcenidali6@gmail.com>
2023-03-17 10:23:34 -0700

    print random speach

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 /**
  * main - Entry point
  *
- * Return: 1
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
     char str[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, str, sizeof(str));          
-    return (1);
+	write(2,str[], sizeof(str[]));
+	return (1);
 }
