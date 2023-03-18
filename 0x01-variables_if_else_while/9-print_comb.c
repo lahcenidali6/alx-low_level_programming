b5406d5bd11217a02bbb66f4814a0ccc51fb6354
lahcenidali <lehcenidali6@gmail.com>
2023-03-18 11:36:10 -0700

    possible combinations of single-digit numbers.

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point, print Programming is like building a multilingual puzzle
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		putchar(32);
+		putchar(',');
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
