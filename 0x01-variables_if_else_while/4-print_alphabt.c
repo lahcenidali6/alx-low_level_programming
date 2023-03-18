f05021b6964187b0687199b3bb32fe059eee3cc4
lahcenidali <lehcenidali6@gmail.com>
2023-03-18 09:33:28 -0700

    prints the alphabet in lowercase, except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point, print Programming is like building a multilingual puzzle
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 97;
+
+	while (i <= 122){
+		if (i != 113 && i != 101)
+		{
+			putchar(i);
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
