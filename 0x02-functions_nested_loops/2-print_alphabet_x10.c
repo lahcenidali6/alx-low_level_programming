167431c7af1649a99f01952e7ac091af31799403
lahcenidali <lehcenidali6@gmail.com>
2023-03-21 07:48:09 -0700

    prints 10 times the alphabet, in lowercase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet-prints the alphabet, in lowercase
+ */
+void print_alphabet(void)
+{
+	int i = 97,j = 0;
+	
+	for (;j<10,j++)
+	{
+		while (i <= 122)
+		{
+			char c = i;
+			_putchar(c);
+			i++;
+		}
+		_putchar('\n'); 
+	}
+}
