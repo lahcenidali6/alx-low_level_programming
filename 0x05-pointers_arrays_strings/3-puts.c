d562d074dcb463b2c7d2ee53ab30300af6500c2c
lahcenidali <lehcenidali6@gmail.com>
2023-03-27 10:24:10 -0700

    function that prints a string, followed by a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+/**
+ * _puts - function that prints a string, followed by a new line
+ * @str: The character to print
+ */
+void _puts(char *str)
+{
+	int i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
