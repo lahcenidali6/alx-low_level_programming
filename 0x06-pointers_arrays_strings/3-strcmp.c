851c747a33e380cc7f17de01182d5db35459a859
lahcenidali6 <125501569+lahcenidali6@users.noreply.github.com>
2023-04-01 00:13:37 +0000

    function that copies a string.

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings.
+ *
+ * @s1: string one
+ * @s2: string two
+ *
+ * Return: result (int)
+ * result > 0 : s1 > s2
+ * result < 0 : s1 < s2
+ * result = 0 : s1 = s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i, result;
+
+	i = 0;
+	while (s1[i] == s2[i] && s1[i] != '\0')
+	{
+		i++;
+	}
+	result = s1[i] - s2[i];
+	return (result);
+}
