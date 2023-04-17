c196c7248f17cca88182c4e2074bab1c667697c2
lahcenidali <lehcenidali6@gmail.com>
2023-04-17 07:53:08 -0700

    Define a new type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,18 +1,17 @@
 #ifndef dog_h
 #define dog_h
+
 /**
- * struct dog - the dog struct
- * @name: the name of dog
- * @age: the age of dog
- * @owner: the name of owner
- *
- * Description: Longer description
+ * struct dog - Dog with a few attributes
+ * @name: The name of the dog as a pointer to the first character of a string
+ * @age: The age of the dog aas a float
+ * @owner: The name of the owner of the dog as a pointer to the first character
+ * of a string
  */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-	/* declare as many members as desired, but the entire structure size must be known to the compiler. */
-}
+};
 #endif
