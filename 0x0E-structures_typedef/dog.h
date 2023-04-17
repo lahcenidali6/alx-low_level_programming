1e2b521e46eaa680c9d578077dbb87de265b6160
lahcenidali <lehcenidali6@gmail.com>
2023-04-17 15:58:15 -0700

    Define a new type dog_t as a new name for the type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,12 +8,12 @@
  * @owner: The name of the owner of the dog as a pointer to the first character
  * of a string
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
